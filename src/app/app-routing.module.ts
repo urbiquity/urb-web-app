12ec68380dccf554f6184c700e5a8899ac13b2ff
George Albert <albert.macatiog@gmail.com>
2019-10-19 10:58:44 +0800

    George > UUI > Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'urbiquity/main',
+    redirectTo: 'urbiquity/main',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BaseComponent } from './layout/base/base.component';
+import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { MapComponent } from './components/map/map.component';
+import { MainComponent } from './views/main/main.component';
+import { LoginComponent } from './views/login/login.component';
+import { FormBuilderComponent } from './components/form-builder/form-builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    SideNavigationComponent,
+    MapComponent,
+    MainComponent,
+    LoginComponent,
+    FormBuilderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
